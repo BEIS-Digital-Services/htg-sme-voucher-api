1d8b51c772087811bdee3b518481448d0487ebd6
paulc1983 <paul.cripps@gmail.com>
2022-05-31 10:59:15 +0100

    fixed request models

--- Beis.HelpToGrow.Api.Voucher/Models/VoucherCancellationRequest.cs
+++ Beis.HelpToGrow.Api.Voucher/Models/VoucherCancellationRequest.cs
@@ -10,19 +10,29 @@
     /// </summary>
     public class VoucherCancellationRequest : IVoucherRequest
     {
-        [Required(ErrorMessage = "registration is required")]
-        [MinLength(1, ErrorMessage = "registration is required")]
+        [Required(ErrorMessage = "Registration is required")]
+        //  [MinLength(1, ErrorMessage = "registration is required")]
+        [MaxLength(50, ErrorMessage = "registration cannot be more than 50 characters")]
         public string Registration { get; set; }
-        [Required(ErrorMessage = "accessCode is required")]
-        [MinLength(1, ErrorMessage = "accessCode is required")]
+        [Required(ErrorMessage = "AccessCode is required")]
+        // [MinLength(1, ErrorMessage = "accessCode is required")]
+        [MaxLength(50, ErrorMessage = "AccessCode cannot be more than 50 characters")]
         public string AccessCode { get; set; }
-        [Required(ErrorMessage = "voucherCode is required")]
-        [MinLength(1, ErrorMessage = "voucherCode is required")]
+        [Required(ErrorMessage = "VoucherCode is required")]
+        //  [MinLength(1, ErrorMessage = "voucherCode is required")]
+        [MaxLength(256, ErrorMessage = "VoucherCode cannot be more than 256 characters")]
         public string VoucherCode { get; set; }
+
+        [Required(ErrorMessage = "CancellationReason is required")]
+        //[MinLength(1, ErrorMessage = "CancellationReason is required")]
+        [MaxLength(500, ErrorMessage = "CancellationReason cannot be more than 500 characters")]
         public string CancellationReason { get; set; }
 
-        public DateTime CancellationDate { get; set;}
-        public DateTime ContractStartDate { get; set;}
+        [Required(ErrorMessage = "CancellationDate is required")]
+        public DateTime CancellationDate { get; set; }
+
+        [Required(ErrorMessage = "ContractStartDate is required")]
+        public DateTime ContractStartDate { get; set; }
 
     }
 }
--- Beis.HelpToGrow.Api.Voucher/Models/VoucherCheckRequest.cs
+++ Beis.HelpToGrow.Api.Voucher/Models/VoucherCheckRequest.cs
@@ -4,27 +4,13 @@ namespace Beis.HelpToGrow.Api.Voucher.Models
     public class VoucherCheckRequest : IVoucherRequest
     {
         [Required(ErrorMessage = "registration is required")]
-        //  [MinLength(1, ErrorMessage = "registration is required")]
-        [MaxLength(50, ErrorMessage = "registration cannot be more than 50 characters")]
+        [MinLength(1, ErrorMessage = "registration is required")]
         public string Registration { get; set; }
         [Required(ErrorMessage = "accessCode is required")]
-        // [MinLength(1, ErrorMessage = "accessCode is required")]
-        [MaxLength(50, ErrorMessage = "AccessCode cannot be more than 50 characters")]
+        [MinLength(1, ErrorMessage = "accessCode is required")]
         public string AccessCode { get; set; }
         [Required(ErrorMessage = "voucherCode is required")]
-        //  [MinLength(1, ErrorMessage = "voucherCode is required")]
-        [MaxLength(256, ErrorMessage = "VoucherCode cannot be more than 256 characters")]
+        [MinLength(1, ErrorMessage = "voucherCode is required")]
         public string VoucherCode { get; set; }
-
-        [Required(ErrorMessage = "CancellationReason is required")]
-        //[MinLength(1, ErrorMessage = "CancellationReason is required")]
-        [MaxLength(500, ErrorMessage = "CancellationReason cannot be more than 500 characters")]
-        public string CancellationReason { get; set; }
-
-        [Required(ErrorMessage = "CancellationDate is required")]
-        public DateTime CancellationDate { get; set; }
-
-        [Required(ErrorMessage = "ContractStartDate is required")]
-        public DateTime ContractStartDate { get; set; }
     }
 }
